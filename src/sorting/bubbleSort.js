cdad44ac26de48cd7cd285abd4da90bec319c9ae
JavierPoduje <javier.poduje@gmail.com>
2022-02-27 12:12:10 -0300

    feature/sorting: add `bubble sort`

---
+++ src/sorting/bubbleSort.js
@@ -0,0 +1,10 @@
+/**
+ * Escribe una función que reciba un array de `integers` como input y retorne
+ * una versión ordenada de manor a menor de este usando el algoritmo `bubble
+ * sort` para hacerlo.
+ */
+const bubbleSort = (array) => {
+  // tu código va aquí...
+}
+
+export default bubbleSort;
