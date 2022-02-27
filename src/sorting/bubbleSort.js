cf796cbad6ac25f724db9770750c66c04fd66828
JavierPoduje <javier.poduje@gmail.com>
2022-02-27 17:09:07 -0300

    feature/sorting: solve `bubble sort`

--- src/sorting/bubbleSort.js
+++ src/sorting/bubbleSort.js
@@ -4,7 +4,23 @@
  * sort` para hacerlo.
  */
 const bubbleSort = (array) => {
-  // tu código va aquí...
+  for (let i = 0; i < array.length; i++) {
+    let somethingWasSwapped = false;
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      const current = array[j];
+      const next = array[j + 1];
+      if (next < current) {
+        swap(array, j);
+        somethingWasSwapped = true;
+      }
+    }
+    if (!somethingWasSwapped) break;
+  }
+  return array;
+}
+
+const swap = (array, idx) => {
+  [array[idx], array[idx + 1]] = [array[idx + 1], array[idx]];
 }
 
 export default bubbleSort;
